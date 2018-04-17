62e82acb06545573600cdc1f3345195dfcf8ee6b
Geoff Hull <geoffhull03@gmail.com>
2018-04-17 15:39:23 -0700

    Using Rest-Client Gem now...

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,32 @@
+require 'httparty'
+require 'json'
+require 'rest-client'
+
 class WelcomeController < ApplicationController
-  def index
-  end
+
+  CLIENT_ID = ENV['client_id']
+  CLIENT_SECRET = ENV['client_secret']
+  REDIRECT_URL = ENV['redirect_uri']
+
+  include HTTParty
+  include JSON
 
   def callback
+    @authorization_code = params["code"]
 
+    RestClient.post('https://app.procore.com/oauth/token', {
+      "grant_type" => "authorization_code",
+      "client_id" => CLIENT_ID,
+      "client_secret" => CLIENT_SECRET,
+      "code" => @authorization_code,
+      "redirect_uri" => REDIRECT_URL
+    }.to_json, content_type: :json, accept: :json)
+      puts response.body
+    #@me_request['authorization'] = "Bearer #{json['access_token']}"
   end
+
+  #def get_me
+  #  RestClient.get('https://app.procore.com/vapid/me', )
+
+  #end
 end
