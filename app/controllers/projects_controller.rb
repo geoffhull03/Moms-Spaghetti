8797e22a2edfe5bc6bf42c36fe187d97c17d37ca
Geoff Hull <geoffhull03@gmail.com>
2018-09-21 11:23:23 -0700

    Figured out JSON problems

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,16 +3,16 @@ require 'json'
 require 'rest-client'
 
 class ProjectsController < WelcomeController
-  alias_method :callback, :resources
-
   include HTTParty
   include JSON
 
-  def resources
+  def callback
     super
+  end
 
+  def resources
+    #callback
     @company_id = params["company_id"]
-    @projects = []
 
     if @access_token
       puts @access_token
@@ -20,9 +20,9 @@ class ProjectsController < WelcomeController
       puts "no access token"
     end
 
-    response = RestClient.get("https://app.procore.com/vapid/projects?company_id=#{@company_id}", {"Authorization: Bearer" => @access_token})
-    projects_response = JSON.parse(response.body)
+    response = RestClient.get("https://app.procore.com/vapid/projects?company_id=#{@company_id}", {"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzUxMiJ9.eyJhaWQiOiI1N2QzZDkxMWZmOTdiODY2NTMzZWFlYzhkZDYxNjhhOWQ5OWI2Yjc1YjA5NWRjODE3YTBiMDE5MmQxOTA2YWQxIiwiZXhwIjoxNTM3NTYwNzA5LCJ1aWQiOjM1ODQ5MDB9.ACZROjmWGz3GiNqERD2AOKr_ReJaJU_RxrAmhDvlD3z-p2Je6RQtZHg6Yq-ZMwZbXEuvnpeD8MxSGwyDZkBTUYgTADba1c6xAvD5nppeYVYeegipzsn77452ysNXPOVW6cUaIHEBeQgGb5tX1kTtrW0bZr37zCMO8fUue4Ei1iCsz6mq"})
+    @projects = JSON.parse(response)
 
-    puts projects_response
+
   end
 end
