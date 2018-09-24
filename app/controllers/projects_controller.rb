313ffc0899037ba67e670f8f2d5c5c2f4a45aa60
Geoff Hull <geoffhull03@gmail.com>
2018-09-24 15:19:46 -0700

    Switched to monthly sandbox env

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,7 +9,7 @@ class ProjectsController < WelcomeController
   def resources
     @company_id = params["company_id"]
 
-    response = RestClient.get("https://app.procore.com/vapid/projects?company_id=#{@company_id}", {"Authorization": "Bearer #{session[:access_token]}"})
+    response = RestClient.get("https://api-sandbox-monthly.procore.com/vapid/projects?company_id=#{@company_id}", {"Authorization": "Bearer #{session[:access_token]}"})
     @projects = JSON.parse(response)
 
       @projects.each do |project|
@@ -17,7 +17,7 @@ class ProjectsController < WelcomeController
         arr.push(project['id'])
         puts arr
 
-        monitoring_resources = RestClient.get("https://app.procore.com/vapid/projects/#{arr[0]}/monitoring_resources", {"Authorization": "Bearer #{session[:access_token]}"})
+        monitoring_resources = RestClient.get("https://api-sandbox-monthly.procore.com/vapid/projects/#{arr[0]}/monitoring_resources", {"Authorization": "Bearer #{session[:access_token]}"})
         @monitoring_resources = JSON.parse(monitoring_resources)
           if @monitoring_resources == []
             puts "#{project['name']} (#{project['id']}) has no monitoring resources, loser"
