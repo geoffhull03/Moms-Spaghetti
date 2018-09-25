f8782a942d2229dcd7971d83ce198088d8a48c81
Geoff Hull <geoffhull03@gmail.com>
2018-09-25 13:35:50 -0700

    All (ugly) data renders in view;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def user_has_token
-  #@access_token = access_token.access_token
-  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,24 +9,16 @@ class ProjectsController < WelcomeController
   def resources
     @company_id = params["company_id"]
 
-    response = RestClient.get("https://api-sandbox-monthly.procore.com/vapid/projects?company_id=#{@company_id}", {"Authorization": "Bearer #{session[:access_token]}"})
+    response = RestClient.get("https://app.procore.com/vapid/projects?company_id=#{@company_id}", {"Authorization": "Bearer #{session[:access_token]}"})
     @projects = JSON.parse(response)
-
+    @monitoring_resources = []
       @projects.each do |project|
-        arr = []
-        arr.push(project['id'])
-        puts arr
+        #arr = []
+        #arr.push(project['id'])
+        monitoring_resources = RestClient.get("https://app.procore.com/vapid/projects/#{project['id']}/monitoring_resources", {"Authorization": "Bearer #{session[:access_token]}"})
+        @monitoring_resources += JSON.parse(monitoring_resources)
 
-        monitoring_resources = RestClient.get("https://api-sandbox-monthly.procore.com/vapid/projects/#{arr[0]}/monitoring_resources", {"Authorization": "Bearer #{session[:access_token]}"})
-        @monitoring_resources = JSON.parse(monitoring_resources)
-          if @monitoring_resources == []
-            puts "#{project['name']} (#{project['id']}) has no monitoring resources, loser"
-          else puts @monitoring_resources
-          end
-
-
-
-
+        puts @monitoring_resources
       end
   end
 end
