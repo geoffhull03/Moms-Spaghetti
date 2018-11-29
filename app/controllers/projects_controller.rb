712de6cba1f2cdf255291a00e0fb14ec09730317
Geoff Hull <geoffhull03@gmail.com>
2018-11-28 16:00:32 -0800

    Small tweaks

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,10 +11,9 @@ class ProjectsController < WelcomeController
 
     response = RestClient.get("https://app.procore.com/vapid/projects?company_id=#{@company_id}", {"Authorization": "Bearer #{session[:access_token]}"})
     @projects = JSON.parse(response)
+    
     @monitoring_resources = []
       @projects.each do |project|
-        #arr = []
-        #arr.push(project['id'])
         monitoring_resources = RestClient.get("https://app.procore.com/vapid/projects/#{project['id']}/monitoring_resources", {"Authorization": "Bearer #{session[:access_token]}"})
         @monitoring_resources += JSON.parse(monitoring_resources)
 
